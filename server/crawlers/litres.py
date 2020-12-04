d38aa818ff535d6988bcccb49baaed3734ce5e28
exp101t <ritter.german@yandex.ru>
2020-12-04 05:35:37 +0300

    extracting info about book

--- server/crawlers/litres.py
+++ server/crawlers/litres.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from typing import Any, Dict, List
 
 import grequests
@@ -12,6 +13,21 @@ _headers = {
     'User-Agent': random.choice(USER_AGENTS)
 }
 
+month_num_mapping = {
+    'января': 1,
+    'февраля': 2,
+    'марта': 3,
+    'апреля': 4,
+    'мая': 5,
+    'июня': 6,
+    'июля': 7,
+    'августа': 8,
+    'сентября': 9,
+    'октября': 10,
+    'ноября': 11,
+    'декабря': 12
+}
+
 
 def async_get(urls: List[str]) -> List[Response]:
     """
@@ -22,7 +38,7 @@ def async_get(urls: List[str]) -> List[Response]:
     return grequests.map(gen, size=_requests_at_time)
 
 
-def extract_author_info(page_urls: List[str]) -> List[Dict[str, Any]]:
+def extract_authors_info(page_urls: List[str]) -> List[Dict[str, Any]]:
     """
         Get and parse info about authors by provided links
     """
@@ -49,7 +65,7 @@ def extract_author_info(page_urls: List[str]) -> List[Dict[str, Any]]:
             photo_path = 'https://litres.ru' + photo_path
 
         authors.append({
-            'author_name': name,
+            'name': name,
             'bio': bio,
             'photo_path': photo_path
         })
@@ -57,15 +73,76 @@ def extract_author_info(page_urls: List[str]) -> List[Dict[str, Any]]:
     return authors
 
 
-def extract_book_info(page_url: List[str]) -> List[Dict[str, Any]]:
-    return {
-        'book_name': None,
-        'rating_sum': 0,
-        'rating_num': 0,
-        'publish_date': None,
-        'preamble': None,
-        'cover_path': None
-    }
+def parse_date(pretty: str) -> datetime:
+    day, month, year = pretty.split()
+
+    return datetime(
+        day=int(day),
+        month=month_num_mapping[month],
+        year=int(year)
+    )
+
+
+def extract_books_info(page_urls: List[str]) -> List[Dict[str, Any]]:
+    """
+        Get and parse info about books by provided links
+    """
+
+    responses = async_get(page_urls)
+    books = []
+
+    for response in responses:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        name = soup.find('div', {'class': 'biblio_book_name'})
+        name = next(name.stripped_strings)
+
+        publish_date = soup.find('strong', string='Дата выхода на ЛитРес:')
+        publish_date = publish_date.next_sibling
+        publish_date = parse_date(publish_date)
+
+        preamble = soup.find('div', {'class': 'biblio_book_descr_publishers'})
+        if preamble is not None:
+            preamble = preamble.get_text(separator='\n')
+
+        cover_path = soup.find('meta', {'property': 'og:image'})['content']
+
+        authors = soup.find('div', {'class': 'biblio_book_author'})
+        authors = authors.find_all('a')
+        authors = extract_authors_info(['https://litres.ru' + link['href']
+                                        for link in authors])
+
+        tags = soup.find('li', {'class': 'tags_list'})
+        if tags is not None:
+            tags = tags.find_all('a', {'class': 'biblio_info__link'})
+            tags = [{'name': tag.text.capitalize()} for tag in tags]
+        else:
+            tags = []
+
+        genres = soup.find('strong', string='Жанр:').parent
+        genres = genres.find_all('a', {'class': 'biblio_info__link'})
+        genres = [{'name': genre.text.capitalize()} for genre in genres]
+
+        # But there may be multiple series...
+        series = soup.find('div', {'class': 'biblio_book_sequences'})
+        if series is not None:
+            series = series.find('a', {'class': 'biblio_book_sequences__link'})
+            series = series.text
+
+        books.append({
+            'name': name,
+            'rating_sum': 0,
+            'rating_num': 0,
+            'publish_date': publish_date,
+            'preamble': preamble,
+            'cover_path': cover_path,
+            'authors': authors,
+            'tags': tags,
+            'genres': genres,
+            'series': series
+        })
+
+    return books
 
 
 if __name__ == '__main__':
