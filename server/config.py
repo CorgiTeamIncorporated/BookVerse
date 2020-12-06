1346cc254de8dfefd066d91b502233c2397e8b4e
Leon Useinov <leon-1402@mail.ru>
2020-12-07 02:06:44 +0300

    repair server/config.py

---
+++ server/config.py
@@ -0,0 +1,9 @@
+DB_NAME = 'bookverse'
+DB_USER = 'app'
+DB_PASS = '1234'
+DB_HOST = 'localhost'
+DB_PORT = 5432
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'bmp', 'gif'}
+STATIC_DIR = '/var/www'
+UPLOAD_DIR = '/static/img'
