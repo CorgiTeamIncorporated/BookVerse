62a9dd7386e8ad7dffdbe20cb8e75344ebc10572
Leon Useinov <leon-1402@mail.ru>
2020-12-21 08:36:42 +0300

    add new line and link path changes

---
+++ server/crawlers/config.py
@@ -0,0 +1,11 @@
+DB_NAME = 'bookverse'
+DB_USER = 'app'
+DB_PASS = 'put your password'
+DB_HOST = 'localhost'
+DB_PORT = 5432
+
+APP_SECRET = 'zvuwZCbHSTSqPOGZmDHq27kqI-uqSj_0KV1C0ypScO0'
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'bmp', 'gif'}
+STATIC_DIR = '/var/www'
+UPLOAD_DIR = '/static/img'
